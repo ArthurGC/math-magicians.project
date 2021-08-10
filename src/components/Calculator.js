822e16cab274cb4e516caa38068b52e818cdb1f0
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-10 15:30:04 -0500

    Add Calculator class

---
+++ src/components/Calculator.js
@@ -0,0 +1,11 @@
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+            <div>Hello React!</div>
+        )
+    }
+}
