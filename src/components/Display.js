3e73fce322b149cb03004f7a766858497f2653c4
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-10 16:34:51 -0500

    Add display component

---
+++ src/components/Display.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Display extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="display">0</div>
+    );
+  }
+}
+
+export default Display;
