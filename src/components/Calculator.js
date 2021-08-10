eed546762f1e3881865d0c3922c0d4d18b119911
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-10 16:35:48 -0500

    Add Calculator component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,11 +1,21 @@
+import React from 'react';
+import Display from './Display';
+import ButtonsTable from './ButtonsTable';
+
 class Calculator extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-    render() {
-        return (
-            <div>Hello React!</div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="main">
+        <Display />
+        <ButtonsTable />
+      </div>
+    );
+  }
 }
+
+export default Calculator;
