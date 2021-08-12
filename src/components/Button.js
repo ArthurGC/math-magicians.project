3c740c78b49f6227b7a1cc9f0e490f98e414b751
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-12 11:38:43 -0500

    Refactor Button to functional

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Button extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    const { color, data } = this.props;
-    return (
-      <button type="button" className={color} data-name={data}>{data}</button>
-    );
-  }
-}
+const Button = ({ color, data }) => (
+  <button type="button" className={color} data-name={data}>{data}</button>
+);
 
 Button.defaultProps = {
   color: 'white',
