5a86d8777cadc66adc882cdefb01766584ea0663
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-12 22:27:47 -0500

    Show error in Display

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Display = ({ input, next, operation }) => (
-  <div className="input" id="display">
-    {input}
-    {' '}
-    {operation}
-    {' '}
-    {next}
-    {' '}
-  </div>
-);
+const Display = ({ input, next, operation }) => {
+  let error = null;
+  if (next === '0' && operation === '÷') {
+    error = 'Division by zero is undefined   ';
+  } else {
+    error = null;
+  }
+  return (
+    <div className="input" id="display">
+      {error}
+      {'     '}
+      {input}
+      {' '}
+      {operation}
+      {' '}
+      {next}
+      {' '}
+    </div>
+  );
+};
 
 Display.defaultProps = {
   input: '',
