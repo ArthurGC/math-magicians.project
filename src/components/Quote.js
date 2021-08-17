b617dc31026f4269b4240f0cd849e4ecd197f02a
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-16 23:42:06 -0500

    Add router-switch-routes

---
+++ src/components/Quote.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Quote = () => (
+  <div>
+    <h2 className="quote__content">Mathematics is not about numbers, equations, computations, or algorithms: It is about understanding. --William Paul Thurston</h2>
+  </div>
+);
+
+export default Quote;
