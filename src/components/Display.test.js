bd5fe428277971f3223555c358cfa6adfbc9662f
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-18 01:27:40 -0500

    Add test to Main and Display components

---
+++ src/components/Display.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render } from '@testing-library/react';
+import Display from './Display';
+
+describe('Test Display component', () => {
+    //Arrange
+    const props = {
+        total: '25',
+        next: '0',
+        operation: '÷',
+    }
+
+    //Act
+    const component = render(<Display input={props.total} next={props.next} operation={props.operation} />);
+    //Assert
+    test('Testing Display render and receive a division by 0', () => {
+        component.getByText('25 ÷ 0');
+        component.getByText('Division by zero is undefined');
+    })
+})
---
+++ src/components/MainContainer.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { fireEvent, render } from '@testing-library/react';
+import MainContainer from './MainContainer';
+
+describe('Test MainContainer component', () => {
+    //Arrange
+    const mockEvent = jest.fn();
+
+    //Act
+    const component = render(<MainContainer event={mockEvent} />)
+    const button = component.getAllByRole('button')[0];
+    //Assert
+    test('Testing event functionality call', () => {
+        fireEvent.click(button);
+        expect(mockEvent).toHaveBeenCalledTimes(1);
+    })
+})
