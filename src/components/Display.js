6435d0941e81071f986e2fa27cf17350fc7aab16
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-18 01:25:21 -0500

    Add test to Display component

--- src/components/Display.js
+++ src/components/Display.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const Display = ({ input, next, operation }) => {
   let error = null;
   if (next === '0' && operation === '÷') {
-    error = 'Division by zero is undefined   ';
+    error = 'Division by zero is undefined';
   } else {
     error = null;
   }
