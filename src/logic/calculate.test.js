5ecde56c6bfa6449684306e9cdefa9a27b31c52c
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-17 23:50:18 -0500

    Add test to operate function

--- src/logic/calculate.test.js
+++ src/logic/calculate.test.js
@@ -2,43 +2,48 @@ import calculate from "./calculate.js";
 
 describe("Calculate functionality Test", () => {
   //Arrange
-  const obj = {
+ let obj = {
     total: null,
     next: null,
     operation: null,
   };
 
   const completedObj = {
-    total: '5',
-    next: '9',
-    operation: 'x',
-  }
+    total: "5",
+    next: "9",
+    operation: "x",
+  };
 
   const operationsUser = {
-      first: '5',
-      second: '+',
-      third: '9',
-      fourth: 'x',
-      fifth: '3',
-      sixth: '-',
-      seventh: '10',
-      eighth: '÷',
-      nineth: '2',
-  }
+    first: "5",
+    second: "+",
+    third: "9",
+    fourth: "x",
+    fifth: "3",
+    sixth: "-",
+    seventh: "10",
+    eighth: "÷",
+    nineth: "2",
+    tenth: "=",
+  };
 
   //Act
   //Assert
-  test('Doing a hard coding multiplication 5 x 9 should be 45', () => {
-    expect(calculate(completedObj, '=')).toEqual({
-        total: '45',
-        next: null,
-        operation: null,
-    })
-  })
+  test("Doing a hard coding multiplication 5 x 9 should be 45", () => {
+    expect(calculate(completedObj, "=")).toEqual({
+      total: "45",
+      next: null,
+      operation: null,
+    });
+  });
 
-  test('Simulating a user interaction 5 + 9 * 3 - 10 / 2', () => {
-      for (const key in operationsUser) {
-         operationsUser[key]
-      }
-  })
+  test("Simulating a user interaction 5 + 9 * 3 - 10 / 2 should be 16", () => {
+    for (const key in operationsUser) {
+      obj = {...obj, ...calculate(obj, operationsUser[key])};
+    }
+
+    let total = obj.total;
+
+    expect(total).toEqual("16");
+  });
 });
---
+++ src/logic/operate.test.js
@@ -0,0 +1,59 @@
+import operate from "./operate.js";
+
+describe("Operate functionality Test", () => {
+    //Arrange
+    const sum = {
+        numberOne: '10',
+        numberTwo: '7',
+        operation: '+',
+    }
+    const sub = {
+        numberOne: '20',
+        numberTwo: '15',
+        operation: '-',
+    }
+    const mult = {
+        numberOne: '5',
+        numberTwo: '7',
+        operation: 'x',
+    }
+    const div = {
+        numberOne: '144',
+        numberTwo: '12',
+        operation: '÷',
+    }
+    const mod = {
+        numberOne: '13',
+        numberTwo: '169',
+        operation: '%',
+    }
+  
+    //Act
+    const resultSum = operate(sum.numberOne, sum.numberTwo, sum.operation);
+    const resultSub = operate(sub.numberOne, sub.numberTwo, sub.operation);
+    const resultMul = operate(mult.numberOne, mult.numberTwo, mult.operation);
+    const resultDiv = operate(div.numberOne, div.numberTwo, div.operation);
+    const resultMod = operate(mod.numberOne, mod.numberTwo, mod.operation);
+    //Assert
+    test("Test 10 plus 7 should be 17", () => {
+        expect(resultSum).toEqual('17');
+    });
+
+    test("Test 20 minus 15 should be 5", () => {
+        expect(resultSub).toEqual('5');
+    });
+
+    test("Test 5 times 7 should be 35", () => {
+        expect(resultMul).toEqual('35');
+    });
+
+    test("Test 144 div 12 should be 12", () => {
+        expect(resultDiv).toEqual('12');
+    });
+
+    test("Test 13 mod 169 should be 13", () => {
+        expect(resultMod).toEqual('13');
+    });
+
+
+  });
