b854f4378a9cc1ed3dcf0dfc7c0290c1bf171c07
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-18 10:43:07 -0500

    Add Button component test

---
+++ src/components/Button.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Button from './Button';
+
+
+describe('Test Button component', () => {
+    //Arrange
+    const props = {
+        color: 'white',
+        data: 'It is a Test'
+    }
+    //Act
+    const component = render(<Button data={props.data} color={props.color}/>);
+
+    //Assert
+    test('Testing Button component render',() => {
+        component.getByText('It is a Test')
+    })
+})
