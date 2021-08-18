9d2a19d50ae92b6b8123b912d1a403529a21f5d6
eroauditore <alfa.fenrir@gmail.com>
2021-08-18 10:09:12 -0500

    Decimal test added for caculate

--- src/logic/calculate.test.js
+++ src/logic/calculate.test.js
@@ -1,20 +1,29 @@
-import calculate from "./calculate";
+import calculate from './calculate';
 
 describe('Test calculate function', () => {
-    //Arrange
-    let obj = {
-        total:null,
-        next: null,
-        operation:null,
-    }
-    const operations = ['5','x','7','+','10','÷','5','='];
-    //Act
-    //Assert
-    test('Testing an user interaction should 9', () =>{
-        operations.forEach(oper => {
-            obj = { ...obj, ...calculate(obj,oper)}
-        })
-        let total = obj.total;
-        expect(total).toEqual('9');
-    })
-})
+  //Arrange
+  let obj = {
+    total: null,
+    next: null,
+    operation: null,
+  };
+  const operations = ['5', 'x', '7', '+', '10', '÷', '5', '='];
+  //Act
+  //Assert
+  test('Testing an user interaction should 9', () => {
+    operations.forEach((oper) => {
+      obj = { ...obj, ...calculate(obj, oper) };
+    });
+    let total = obj.total;
+    expect(total).toEqual('9');
+  });
+
+  test('Testing an user interaction should 6.5', () => {
+    const operations = ['5', '.', '1', '+', '1', '.', '4', '='];
+    operations.forEach((oper) => {
+      obj = { ...obj, ...calculate(obj, oper) };
+    });
+    let total = obj.total;
+    expect(total).toEqual('6.5');
+  });
+});
